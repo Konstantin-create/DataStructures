24a216a0e8076a1e62f9951f1b06bf35161ece74
Konstantin-create <schahov.konst@gmail.com>
2022-08-01 11:02:12 +0300

    Add get_depth function. Add field to save data

--- BinaryTree/tree.py
+++ BinaryTree/tree.py
@@ -1,9 +1,8 @@
-# Only for digits
 class BinaryTree:
     def __init__(self):
         self.root = Node(index=0)
 
-    def append(self, index: int):
+    def append(self, index: int, data: any):
         """Function to append digit to tree"""
         current_node = self.root
         while True:
@@ -11,16 +10,17 @@ class BinaryTree:
                 raise TypeError(f'Your index is not unic')
             if index < current_node.index:
                 if current_node.left is None:
-                    current_node.left = Node(index=index)
+                    current_node.left = Node(index=index, data=data)
                     return
                 current_node = current_node.left
             elif index > current_node.index:
                 if current_node.right is None:
-                    current_node.right = Node(index=index)
+                    current_node.right = Node(index=index, data=data)
                     return
                 current_node = current_node.right
 
     def find(self, index):
+        """Function to find Node by index"""
         current_node = self.root
         while True:
             if current_node is None:
@@ -32,15 +32,28 @@ class BinaryTree:
             elif index > current_node.index:
                 current_node = current_node.right
 
+    def get_depth(self):
+        """function to find the depth of a tree"""
+        layer = 0
+        current_node = self.root
+        while True:
+            if current_node.left == None:
+                break
+            layer += 1
+            current_node = current_node.left
+        return layer
+            
+
     def __repr__(self):
         return f'{self.root}'
 
 
 class Node:
-    def __init__(self, index=None, left=None, right=None):
+    def __init__(self, index=None, data=None, left=None, right=None):
         self.index = index
+        self.data = data
         self.left = left
         self.right = right
 
     def __repr__(self):
-        return f'({self.index}, {self.left}, {self.right})'
+        return f'({self.index}, {self.data}, {self.left}, {self.right})'
